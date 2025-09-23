6415e2c13f6c93c971b36614a4d0f859c8f95101
Youssef Hashish <yousfhashish0@gmail.com>
2025-09-23 14:05:31 +0300

    items centered

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,15 +12,14 @@ export default async function Home() {
     <>
       <h1 className="text-3xl font-bold mb-5 text-blue-950">Welcome to Biko <span className="text-orange-400">Plaza</span></h1>
       <MainSlider/>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 pt-2">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 pt-2 justify-items-center">
         <Suspense fallback={<HomeLoading/>}>
-        {
-          productList.map((product)=>(
-            <ProductCard key={product._id} product={product}/>
-          ))
-        }
+          {productList.map((product) => (
+            <ProductCard key={product._id} product={product} />
+          ))}
         </Suspense>
       </div>
+
     </>
   );
 }
